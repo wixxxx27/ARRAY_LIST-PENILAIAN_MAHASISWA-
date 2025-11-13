261e149e8249b7dd0570c3eb1e1f128758df553c
Dwiyandra Raysha Putra Syawal <dwiyandraraysha@gmail.com>
2025-11-13 10:33:42 +0700

    Update DataMahasiswa.java

--- DataMahasiswa.java
+++ DataMahasiswa.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package TUGAS_PERTEMUAN9;
+package TUGAS_PERTEMUAN10;
 
 import java.util.ArrayList;
 
@@ -25,3 +25,4 @@ public class DataMahasiswa {
         return list;
     }
 }
+
