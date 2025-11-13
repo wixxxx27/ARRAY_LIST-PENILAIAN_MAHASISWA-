f9a8ac0077a480e71a1dc1cc383fb0aa6bd53530
Dwiyandra Raysha Putra Syawal <dwiyandraraysha@gmail.com>
2025-11-13 10:33:26 +0700

    PENGHITUNG_NILAI.java

--- PENGHITUNG_NILAI.java
+++ PENGHITUNG_NILAI.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package TUGAS_PERTEMUAN9;
+package TUGAS_PERTEMUAN10;
 
 
 import java.io.BufferedReader;
